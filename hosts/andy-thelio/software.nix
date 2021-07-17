c2e91c1e67db88f3cc482a40918c6a3a42229cce
Andrew Hodges <betawaffle@gmail.com>
2021-07-17 07:51:32 -0400

    Remove zulip, add nix-index and zeal

--- hosts/andy-thelio/software.nix
+++ hosts/andy-thelio/software.nix
@@ -64,9 +64,6 @@
     # Firefox, but for Wayland.
     firefox-wayland
 
-    # Communication
-    zulip
-
     # Blender, for 3D modeling.
     blender
 
@@ -87,7 +84,9 @@
     bluez
     evtest
     hid-listen
+    nix-index
     tmate
+    zeal
 
     # Games
     minecraft
@@ -119,6 +118,21 @@
   # I use fish as my shell. This installs it and does some other stuff to make
   # it an allowed shell on NixOS.
   programs.fish.enable = true;
+
+  # Use nix-index for command-not-found behavior, since we don't have a root
+  # channel.
+  programs.command-not-found.enable = false;
+  programs.fish.shellInit = let
+    wrapper = pkgs.writeScript "command-not-found" ''
+      #!${pkgs.bash}/bin/bash
+      source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
+      command_not_found_handle "$@"
+    '';
+  in ''
+    function __fish_command_not_found_handler --on-event fish_command_not_found
+      ${wrapper} $argv
+    end
+  '';
 
   # Less (the alternative to more)
   programs.less.enable = true;
